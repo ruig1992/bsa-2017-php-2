22274a5835503ad2badb83fab1b4323ac273063c
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-05-30 13:40:10 +0000

    Added another solution for task 5

--- src/task5.sql
+++ src/task5.sql
@@ -1,1 +1,3 @@
-SELECT * FROM users WHERE id NOT IN (SELECT b.user_id FROM bookings AS b INNER JOIN tickets AS t ON b.ticket_id = t.id WHERE t.country = 'Aruba');
+SELECT u.* FROM users AS u LEFT JOIN bookings AS b ON u.id = b.user_id LEFT JOIN tickets AS t ON b.ticket_id = t.id AND t.country = 'Aruba' GROUP BY u.id HAVING COUNT(t.id) = 0;
+# OR WITH USING SUBQUERY
+# SELECT * FROM users WHERE id NOT IN (SELECT b.user_id FROM bookings AS b INNER JOIN tickets AS t ON b.ticket_id = t.id WHERE t.country = 'Aruba');
